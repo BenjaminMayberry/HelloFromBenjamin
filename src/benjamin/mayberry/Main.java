069581051dec363ba875608c4712b0f328a3b7fb
Zac <zjensen1022@gmail.com>
2021-10-16 14:24:34 -0600

    Add greeting and change to println for formatting

--- src/benjamin/mayberry/Main.java
+++ src/benjamin/mayberry/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, my name is Benjamin Mayberry!");
         System.out.println("Hello Benjamin Mayberry, My name is Zac Jensen.");
+
     }
 }
